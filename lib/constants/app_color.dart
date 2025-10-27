2ca9fd30f7799a284a3d600e46fa237871dfd798
user <user@Shefiqs-Macbook-Pro.local>
2025-10-27 12:12:48 +0100

    Second commit

---
+++ lib/constants/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color primary = Color(0xff161622);
+  static const Color grey = Color(0xff7e848D);
+  static const Color black = Color(0xff1E1E2D);
+  static const Color blue = Color(0xff0066FF);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color darkGrey = Color(0xffA2A2A7);
+
+
+}
