ac8f17ec3478ecd0d2e994c7248bdb7da8dfeca3
user <owoadeshefiq2277@gmail.com>
2025-10-29 11:54:00 +0100

    saved

--- lib/screens/log_in.dart
+++ lib/screens/log_in.dart
@@ -1,8 +1,3 @@
-import 'package:bank_app/core/constants/constant.dart';
-import 'package:bank_app/core/extensions/num_extension.dart';
-import 'package:bank_app/core/extensions/theme_extensions.dart';
-import 'package:bank_app/screens/widgets/app_textfield.dart';
-import 'package:bank_app/screens/widgets/widget.dart';
 import 'package:flutter/material.dart';
 
 class LogIn extends StatelessWidget {
--- lib/screens/widgets/home.dart
+++ lib/screens/widgets/home.dart
@@ -1,4 +1,5 @@
 import 'package:bank_app/core/constants/constant.dart';
+import 'package:bank_app/screens/widgets/image_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,20 +13,69 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(padding: 
-      const EdgeInsets.symmetric(horizontal: 20.0),
-      child: Row
-      (
-        children: [
-          CircleAvatar(
-            radius: 30,
-            backgroundColor: AppColor.primary,
-          )
-        ],
-        
+      backgroundColor: AppColor.primary,
 
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                  },
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: AppColor.grey,
+                    child: Icon(
+                      Icons.person,
+                      color: AppColor.white,
+                      size: 30,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(width: 12), 
+
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome back",
+                      style: TextStyle(
+                        color: AppColor.darkGrey,
+                        fontSize: 14,
+                      ),
+                    ),
+                    const SizedBox(height: 2),
+                    Text(
+                      "Tanya\nMyroniuk",
+                      style: TextStyle(
+                        color: AppColor.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        height: 1.05,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 30),
+
+            ImageWidget(imagePath: AppImages.card),
+          ],
+          
+
+
+        ),
       ),
     );
   }
